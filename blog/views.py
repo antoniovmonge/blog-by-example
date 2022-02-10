8ae1cfa818277317130730bb80ef5bc66fec88c8
Antonio Vilchez Monge <antoniovmonge@gmail.com>
2022-02-10 19:52:56 +0100

    created basic search engine

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post, Comment
 from django.views.generic import ListView
-from .forms import EmailPostForm, CommentForm
+from django.contrib.postgres.search import SearchVector
+from .forms import EmailPostForm, CommentForm, SearchForm
 from django.core.mail import send_mail
 from taggit.models import Tag
 
@@ -29,7 +30,8 @@ def post_list(request, tag_slug=None):
         posts = paginator.page(paginator.num_pages)
 
     return render(
-        request, "blog/post/list.html", {"page": page, "posts": posts, "tag": tag}
+        request, "blog/post/list.html", {"page": page,
+                                         "posts": posts, "tag": tag}
     )
 
 
@@ -60,7 +62,8 @@ def post_detail(request, year, month, day, post):
 
     # List of similar posts
     post_tags_ids = post.tags.values_list("id", flat=True)
-    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = Post.published.filter(
+        tags__in=post_tags_ids).exclude(id=post.id)
     similar_posts = similar_posts.annotate(same_tags=Count("tags")).order_by(
         "-same_tags", "-publish"
     )[:4]
@@ -105,7 +108,8 @@ def post_share(request, post_id):
                 f"{cd['name']}'s comments: {cd['comments']}"
             )
 
-            send_mail(subject, message, "pythondeveloperberlin@gmail.com", [cd["to"]])
+            send_mail(subject, message,
+                      "pythondeveloperberlin@gmail.com", [cd["to"]])
 
             sent = True
 
@@ -113,5 +117,23 @@ def post_share(request, post_id):
         form = EmailPostForm()
 
     return render(
-        request, "blog/post/share.html", {"post": post, "form": form, "sent": sent}
+        request, "blog/post/share.html", {"post": post,
+                                          "form": form, "sent": sent}
     )
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = Post.published.annotate(
+                search=SearchVector('title', 'body'),).filter(search=query)
+    return render(request,
+                  'blog/post/search.html',
+                  {'form': form,
+                   'query': query,
+                   'results': results})
